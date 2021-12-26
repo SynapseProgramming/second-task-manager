f0d1f61646acc85c4fcd70d8bb873f6d37674302
Roald Ong <roald.ong@gmail.com>
2021-12-27 00:33:53 +0800

    Replaced link to bookeeper page using bootstrap button

--- app/javascript/components/home.jsx
+++ app/javascript/components/home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import {Outlet, Link} from "react-router-dom";
-import {Navbar, Nav} from "react-bootstrap";
-import {LinkContainer} from "react-router-bootstrap";
 
 export default () => (
 	<div>
@@ -10,18 +8,17 @@ export default () => (
 				Task Planner V1.0
 			</a>
 			<form class="form-inline">
-				<a class="btn btn-outline-success" role="button">
-					View Tasks
-				</a>
+				<Link to="/hehe">
+					<button class="btn btn-outline-success" type="button">
+						View Tasks
+					</button>
+				</Link>
 				<button class="btn btn-sm btn-outline-secondary" type="button">
 					Add Tasks
 				</button>
 				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">
 					Search
 				</button>
-				<LinkContainer to="/hehe">
-					<Nav.Link>BooKeeper</Nav.Link>
-				</LinkContainer>
 			</form>
 		</nav>
 		<Outlet />
