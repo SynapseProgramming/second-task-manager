53a12ddc176ef1eaf1fcac3b040b60fa8ea7d123
Roald Ong <roald.ong@gmail.com>
2021-12-28 00:52:39 +0800

    Added in bootstrap table to display tasks

--- app/javascript/components/tasktable.js
+++ app/javascript/components/tasktable.js
@@ -5,19 +5,45 @@ const SimpleTable = props => {
 	return (
 		<div>
 			<BootstrapTable data={props.data}>
-				<TableHeaderColumn isKey dataField="id" />
-				<TableHeaderColumn dataField="task" />
-				<TableHeaderColumn dataField="priority" />
-				<TableHeaderColumn dataField="description" />
+				<TableHeaderColumn isKey dataField="id" dataAlign="center">
+					Id
+				</TableHeaderColumn>
+				<TableHeaderColumn dataField="task" dataAlign="center">
+					Task
+				</TableHeaderColumn>
+				<TableHeaderColumn dataField="priority" dataAlign="center">
+					Priority
+				</TableHeaderColumn>
+				<TableHeaderColumn dataField="description" dataAlign="center">
+					Description
+				</TableHeaderColumn>
 			</BootstrapTable>
 		</div>
 	);
 };
 
-export default function App() {
-	return (
-		<div>
-			<h1>Task Table</h1>
-		</div>
-	);
+class Tasks extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			tasks: []
+		};
+	}
+
+	componentDidMount() {
+		const url = "/api/v1/tasks/index";
+		fetch(url)
+			.then(response => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw new Error("Network response was not ok.");
+			})
+			.then(response => this.setState({tasks: response}))
+			.catch(() => this.props.history.push("/"));
+	}
+	render() {
+		return <SimpleTable data={this.state.tasks} />;
+	}
 }
+export default Tasks;
