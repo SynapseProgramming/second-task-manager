284f7369c095ce942ab6ca89514541ed48ac5024
Roald Ong <roald.ong@gmail.com>
2021-12-27 12:21:16 +0800

    set title as text only

--- app/javascript/components/home.jsx
+++ app/javascript/components/home.jsx
@@ -4,9 +4,7 @@ import {Outlet, Link} from "react-router-dom";
 export default () => (
 	<div>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="#">
-				Task Planner V1.0
-			</a>
+			<span class="navbar-brand mb-0 h1">Task Planner V1.0</span>
 			<form class="form-inline">
 				<Link to="/hehe">
 					<button class="btn btn-outline-success" type="button">
