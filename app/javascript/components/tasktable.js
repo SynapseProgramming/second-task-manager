3fb6521e1c23b3e848d4b67f4b4d2270b6fd026f
Roald Ong <roald.ong@gmail.com>
2021-12-28 00:34:23 +0800

    Installed bootstrap table, and created simple table function

--- app/javascript/components/tasktable.js
+++ app/javascript/components/tasktable.js
@@ -1,4 +1,18 @@
-import React from "react";
+import React, {Component} from "react";
+import {BootstrapTable, TableHeaderColumn} from "react-bootstrap-table";
+
+const SimpleTable = props => {
+	return (
+		<div>
+			<BootstrapTable data={props.data}>
+				<TableHeaderColumn isKey dataField="id" />
+				<TableHeaderColumn dataField="task" />
+				<TableHeaderColumn dataField="priority" />
+				<TableHeaderColumn dataField="description" />
+			</BootstrapTable>
+		</div>
+	);
+};
 
 export default function App() {
 	return (
