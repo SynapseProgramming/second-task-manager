e0ef0ebe4e56efe9022e6f4f10b241b6728fbf62
Roald Ong <roald.ong@gmail.com>
2021-12-27 15:09:37 +0800

    Migrated data to database and added in null checks.

--- db/migrate/20211227065728_create_tasks.rb
+++ db/migrate/20211227065728_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
-      t.string :task
-      t.string :priority
-      t.text :description
+      t.string :task, null:false
+      t.string :priority, null:false
+      t.text :description, null:false
 
       t.timestamps
     end
