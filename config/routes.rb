5a613a8433d00ef215b6c7dae736ea575a5cbc5a
Roald Ong <roald.ong@gmail.com>
2021-12-27 15:22:51 +0800

    Added in tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'tasks/index'
+      get 'tasks/create'
+      get 'tasks/show'
+      get 'tasks/destroy'
+    end
+  end
   root 'pages#home'
   get 'pages/todo_items'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
