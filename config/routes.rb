373f32d893b93698964b603a7a50d502cb525a83
Roald Ong <roald.ong@gmail.com>
2021-12-28 13:59:57 +0800

    Removed redundant functions

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,5 @@ Rails.application.routes.draw do
     end
   end
   root 'pages#home'
-  get 'pages/todo_items'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
