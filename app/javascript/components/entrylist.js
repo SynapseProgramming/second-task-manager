10a13a8748af0af289dec747152f97afafebdd46
Roald Ong <roald.ong@gmail.com>
2021-12-29 14:25:47 +0800

    Completed form input fields

--- app/javascript/components/entrylist.js
+++ app/javascript/components/entrylist.js
@@ -6,20 +6,21 @@ import Button from "react-bootstrap/Button";
 export default function App() {
 	return (
 		<Form>
-			<Form.Group className="mb-3" controlId="formBasicEmail">
-				<Form.Label>Email address</Form.Label>
-				<Form.Control type="email" placeholder="Enter email" />
-				<Form.Text className="text-muted">
-					We'll never share your email with anyone else.
-				</Form.Text>
+			<Form.Group className="mb-3" controlId="TaskInput">
+				<Form.Label>Task</Form.Label>
+				<Form.Control type="textarea" placeholder="Enter Task" rows={3} />
 			</Form.Group>
-
-			<Form.Group className="mb-3" controlId="formBasicPassword">
-				<Form.Label>Password</Form.Label>
-				<Form.Control type="password" placeholder="Password" />
+			<Form.Group className="mb-2" controlId="PriorityInput">
+				<Form.Label>Priority</Form.Label>
+				<Form.Select>
+					<option value="1">High</option>
+					<option value="2">Medium</option>
+					<option value="3">Low</option>
+				</Form.Select>{" "}
 			</Form.Group>
-			<Form.Group className="mb-3" controlId="formBasicCheckbox">
-				<Form.Check type="checkbox" label="Check me out" />
+			<Form.Group className="mb-3" controlId="DescriptionInput">
+				<Form.Label>Description</Form.Label>
+				<Form.Control as="textarea" rows={3} />
 			</Form.Group>
 			<Button variant="primary" type="submit">
 				Submit
