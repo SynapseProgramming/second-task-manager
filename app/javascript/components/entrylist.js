69d8ce3fa12e0dba35ffa0618eee747427ad7de7
Roald Ong <roald.ong@gmail.com>
2021-12-29 16:00:32 +0800

    Changing specific fields of forms would be passed as an event to onChange

--- app/javascript/components/entrylist.js
+++ app/javascript/components/entrylist.js
@@ -9,33 +9,43 @@ export default class Taskinput extends Component {
 		this.state = {};
 		this.onChange = this.onChange.bind(this);
 	}
-	change_cnt = 0;
 	onChange(event) {
 		console.log("something has changed");
-		console.log(this.change_cnt);
-		this.change_cnt = this.change_cnt + 1;
+		console.log(event.target.name);
+		console.log(event.target.value);
 	}
 	do_stuff = () => {
 		console.log("button pressed");
 	};
 	render() {
 		return (
-			<Form onChange={this.onChange}>
+			<Form>
 				<Form.Group className="mb-3" controlId="TaskInput">
 					<Form.Label>Task</Form.Label>
-					<Form.Control type="textarea" placeholder="Enter Task" rows={3} />
+					<Form.Control
+						type="textarea"
+						placeholder="Enter Task"
+						rows={3}
+						name="task"
+						onChange={this.onChange}
+					/>
 				</Form.Group>
 				<Form.Group className="mb-2" controlId="PriorityInput">
 					<Form.Label>Priority</Form.Label>
-					<Form.Select>
-						<option value="1">High</option>
-						<option value="2">Medium</option>
-						<option value="3">Low</option>
+					<Form.Select name="priority" onChange={this.onChange}>
+						<option value="High">High</option>
+						<option value="Medium">Medium</option>
+						<option value="Low">Low</option>
 					</Form.Select>{" "}
 				</Form.Group>
 				<Form.Group className="mb-3" controlId="DescriptionInput">
 					<Form.Label>Description</Form.Label>
-					<Form.Control as="textarea" rows={3} />
+					<Form.Control
+						name="description"
+						onChange={this.onChange}
+						as="textarea"
+						rows={3}
+					/>
 				</Form.Group>
 				<Button variant="primary" onClick={() => this.do_stuff()}>
 					Submit
