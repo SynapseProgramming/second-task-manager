5d9c0247dc7f24013db16a3822569156ac7a93bd
Roald Ong <roald.ong@gmail.com>
2021-12-31 17:25:30 +0800

    re-written task input as a functional component

--- app/javascript/components/entrylist.js
+++ app/javascript/components/entrylist.js
@@ -2,98 +2,114 @@ import React, {Component} from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import * as Yup from "yup";
-import {Formik} from "formik";
+import {yup} from "yup";
+import {useFormik} from "formik";
 
-const schema = yup.object().shape({
-	task: yup.string().required(),
-	description: yup.string().required()
-});
+//
+// export default class Taskinput extends Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			task: "",
+// 			priority: "High",
+// 			description: ""
+// 		};
+//
+// 		this.onChange = this.onChange.bind(this);
+// 		this.onSubmit = this.onSubmit.bind(this);
+// 	}
+// 	onChange(event) {
+// 		this.setState({[event.target.name]: event.target.value});
+// 	}
+// 	onSubmit(event) {
+// 		// prevent default html redirect
+// 		event.preventDefault();
+// 		const {task, priority, description} = this.state;
+// 		// if theres no data filled inside
+// 		if (task.length == 0 || description.length == 0) return;
+// 		//simple json display
+// 		const body = {
+// 			task,
+// 			priority,
+// 			description
+// 		};
+// 		const token = document.querySelector('meta[name="csrf-token"]').content;
+// 		const url = "api/v1/tasks/create";
+// 		fetch(url, {
+// 			method: "POST",
+// 			headers: {
+// 				"X-CSRF-Token": token,
+// 				"Content-Type": "application/json"
+// 			},
+// 			body: JSON.stringify(body)
+// 		})
+// 			.then(response => {
+// 				if (response.ok) {
+// 					return response.json();
+// 				}
+// 				throw new Error("Network response was not ok.");
+// 			})
+// 			.then(response => {
+// 				//TODO: Add a popup which shows that the submission is successful
+// 				// TODO: Add data checks for the input fields.
+// 				console.log("Successfully Added");
+// 			})
+// 			.catch(error => console.log(error.message));
+// 	}
+//
 
-export default class Taskinput extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			task: "",
-			priority: "High",
-			description: ""
-		};
+//
+// 	render() {
 
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
-	onChange(event) {
-		this.setState({[event.target.name]: event.target.value});
-	}
-	onSubmit(event) {
-		// prevent default html redirect
-		event.preventDefault();
-		const {task, priority, description} = this.state;
-		// if theres no data filled inside
-		if (task.length == 0 || description.length == 0) return;
-		//simple json display
-		const body = {
-			task,
-			priority,
-			description
-		};
-		const token = document.querySelector('meta[name="csrf-token"]').content;
-		const url = "api/v1/tasks/create";
-		fetch(url, {
-			method: "POST",
-			headers: {
-				"X-CSRF-Token": token,
-				"Content-Type": "application/json"
-			},
-			body: JSON.stringify(body)
-		})
-			.then(response => {
-				if (response.ok) {
-					return response.json();
-				}
-				throw new Error("Network response was not ok.");
-			})
-			.then(response => {
-				//TODO: Add a popup which shows that the submission is successful
-				// TODO: Add data checks for the input fields.
-				console.log("Successfully Added");
-			})
-			.catch(error => console.log(error.message));
-	}
+// 	}
+// }
 
-	render() {
-		return (
-			<Form noValidate onSubmit={this.onSubmit}>
-				<Form.Group className="mb-3" controlId="TaskInput">
-					<Form.Label>Task</Form.Label>
-					<Form.Control
-						type="textarea"
-						placeholder="Enter Task"
-						rows={3}
-						name="task"
-						onChange={this.onChange}
-					/>
-				</Form.Group>
-				<Form.Group className="mb-2" controlId="PriorityInput">
-					<Form.Label>Priority</Form.Label>
-					<Form.Select name="priority" onChange={this.onChange}>
-						<option value="High">High</option>
-						<option value="Medium">Medium</option>
-						<option value="Low">Low</option>
-					</Form.Select>{" "}
-				</Form.Group>
-				<Form.Group className="mb-3" controlId="DescriptionInput">
-					<Form.Label>Description</Form.Label>
-					<Form.Control
-						name="description"
-						type="text"
-						onChange={this.onChange}
-						as="textarea"
-						rows={3}
-					/>
-				</Form.Group>
-				<Button type="submit">Submit</Button>
-			</Form>
-		);
-	}
-}
+// const schema = yup.object().shape({
+// 	task: yup.string().required(),
+// 	description: yup.string().required()
+// });
+
+const TaskInput = () => {
+	const formik = useFormik({
+		initialValues: {task: "", priority: "High", description: ""},
+		onSubmit: values => {
+			alert(JSON.stringify(values, null, 1));
+		}
+	});
+
+	return (
+		<Form noValidate onSubmit={formik.handleSubmit}>
+			<Form.Group className="mb-3" controlId="TaskInput">
+				<Form.Label>Task</Form.Label>
+				<Form.Control
+					type="textarea"
+					placeholder="Enter Task"
+					rows={3}
+					name="task"
+					onChange={formik.handleChange}
+				/>
+			</Form.Group>
+			<Form.Group className="mb-2" controlId="PriorityInput">
+				<Form.Label>Priority</Form.Label>
+				<Form.Select name="priority" onChange={formik.handleChange}>
+					<option value="High">High</option>
+					<option value="Medium">Medium</option>
+					<option value="Low">Low</option>
+				</Form.Select>{" "}
+			</Form.Group>
+			<Form.Group className="mb-3" controlId="DescriptionInput">
+				<Form.Label>Description</Form.Label>
+				<Form.Control
+					name="description"
+					type="text"
+					onChange={formik.handleChange}
+					as="textarea"
+					rows={3}
+				/>
+			</Form.Group>
+			<Button type="submit">Submit</Button>
+		</Form>
+	);
+};
+
+export default TaskInput;
