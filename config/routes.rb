59ecd0e71b58a6e156532b3ac41b7bfa33a57e0a
Roald Ong <roald.ong@gmail.com>
2022-01-01 20:21:21 +0800

    fixed routing issue where refreshing the page would bring about a get error.

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
     end
   end
   root 'pages#home'
+  get '*path', to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
