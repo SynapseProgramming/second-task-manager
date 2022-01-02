2e7bb5bab346f1cbe498ed23ceb546f6a654bb4e
Roald Ong <roald.ong@gmail.com>
2022-01-02 17:14:51 +0800

    added in remainingTasks state for cards

--- app/javascript/components/cards.js
+++ app/javascript/components/cards.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "react-bootstrap/Card";
 
-export default () => (
-	<div>
-		<Card border="primary" className="text-center">
-			<Card.Header>Remaining Tasks</Card.Header>
-			<Card.Body>
-				<Card.Title>You have 6 Tasks Remaining!</Card.Title>
-				<Card.Text>Remember to take frequent breaks :)</Card.Text>
-			</Card.Body>
-			<Card.Footer className="text-muted">Never gonna give you up!</Card.Footer>
-		</Card>
-	</div>
-);
+const ShowCards = () => {
+	const [remainingTasks, setremainingTasks] = useState(0);
+
+	return (
+		<div>
+			<Card border="primary" className="text-center">
+				<Card.Header>Remaining Tasks</Card.Header>
+				<Card.Body>
+					<Card.Title>You have {remainingTasks} Tasks remaining!</Card.Title>
+					<Card.Text>Remember to take frequent breaks :)</Card.Text>
+				</Card.Body>
+				<Card.Footer className="text-muted">
+					Never gonna give you up!
+				</Card.Footer>
+			</Card>
+		</div>
+	);
+};
+
+export default ShowCards;
